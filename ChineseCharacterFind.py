3f2544f3b1c15ae2e75a8cc5abd5258d1a868e39
yao xiao <xjywt123@gmail.com>
2016-06-10 21:14:32 +0800

    Update ChineseCharacterFind.py

--- ChineseCharacterFind.py
+++ ChineseCharacterFind.py
@@ -2,7 +2,7 @@ import re
 import sys
 def ChineseCharacterFind():
     f=open(r'C:\Users\Administrator\Desktop\hello.txt','r',encoding='utf-8');
-    pattern=re.compile(r'[\u4e00-\u9fa5]+')
+    pattern=re.compile(r'[\u4e00-\u9fa5]+')# in UTF-8 the Chinese Character's code is u4e00-u9fa5
     try:
         text=f.read();
         array=pattern.findall(text)

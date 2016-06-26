810c475396c7ed32f3fe27df7e83a0198506760b
yao xiao <xjywt123@gmail.com>
2016-06-26 21:18:50 +0800

    date2weekday

---
+++ DateTime2WeekDay.py
@@ -0,0 +1,16 @@
+__author__ = 'Administrator'
+import re
+str="output_2016.06.26.txt"
+x=re.search("output_(?P<Year>\d{4}).(?P<Month>\d{2}).(?P<Day>\d{2})",str)
+Year=int(x.group(1))
+y=Year%100
+print(y)
+C=int(Year/100)
+print(C)
+m=int(x.group(2))
+print(m)
+d=int(x.group(3))
+print(d)
+W=int(C/4)-2*C+y+int(y/4)+int(26*(m+1)/10)+d-1
+print(W%7)
+
